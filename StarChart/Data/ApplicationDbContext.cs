b5e30a65b5f440a1398adc7c70bb11043082523f
JulieBurger81 <61249866+JulieBurger81@users.noreply.github.com>
2020-06-01 16:20:42 -0500

    .Net Beg Mod 5 - Step 3 - changing spaces to see if this works?

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
         public DbSet<CelestialObject> CelestialObjects { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
